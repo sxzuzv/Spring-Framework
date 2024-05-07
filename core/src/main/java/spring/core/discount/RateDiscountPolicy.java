41204b427893b32ce6ae503140a292da09c3c89d
soojin <pofoxz@gmail.com>
2024-05-07 23:14:35 +0900

    Feat: 주문 금액 대비 10% 할인 정책

---
+++ core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package spring.core.discount;
+
+import spring.core.member.Grade;
+import spring.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    // 주문한 금액 대비 %를 할인해주는 새로운 정률 할인 정책을 추가한다.
+    private int discountPercent = 10;   // 할인율 10% 고정
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
