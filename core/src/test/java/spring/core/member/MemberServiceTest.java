037bb3fc924b085f2a4bec08d3bbf9a2241727c9
soojin <pofoxz@gmail.com>
2024-05-08 16:28:22 +0900

    Test: AppConfig 클래스 적용

--- core/src/test/java/spring/core/member/MemberServiceTest.java
+++ core/src/test/java/spring/core/member/MemberServiceTest.java
@@ -1,14 +1,24 @@
 package spring.core.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.core.AppConfig;
 
 /**
  * Junit 테스트 프레임워크를 활용한 테스트 코드를 작성한다.
  */
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    // MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach // 테스트 실행 전, 무조건 실행
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
--- core/src/test/java/spring/core/order/OrderServiceTest.java
+++ core/src/test/java/spring/core/order/OrderServiceTest.java
@@ -1,15 +1,26 @@
 package spring.core.order;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.core.AppConfig;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
-import spring.core.member.MemberServiceImpl;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    // MemberService memberService = new MemberServiceImpl();
+    // OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach // 테스트 실행 전, 무조건 실행
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
