9c500f8d58eb248b3c1e1c07c90205a8956b64b4
soojin <pofoxz@gmail.com>
2024-05-10 22:46:21 +0900

    Refactor: @Configuration, @Bean 적용

--- core/src/main/java/spring/core/AppConfig.java
+++ core/src/main/java/spring/core/AppConfig.java
@@ -1,7 +1,8 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.core.discount.DiscountPolicy;
-import spring.core.discount.FixDiscountPolicy;
 import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
@@ -14,7 +15,16 @@ import spring.core.order.OrderServiceImpl;
  * => 역할과 구현이 명확히 드러나도록 작성한다.
  */
 
+/**
+ * @Configuration: 스프링 컨테이너는 해당 애너테이션이 붙은 클래스를 설정(구성) 정보로 사용한다.
+ * @Bean: 이때, @Bean 애너테이션이 붙은 메서드를 모두 호출, 반환된 객체를 스프링 컨테이너에 등록한다.
+ * => 위의 과정을 거쳐 스프링 컨테이너에 등록된 객체를 '스프링 빈'이라고 명칭한다.
+ * 스프링 빈은 @Bean 애너테이션이 붙은 메서드의 이름을 스프링 빈의 이름으로 사용한다.
+ */
+
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         // 1) MemoryMemberRepositoryImpl 객체를 생성한다.
         // 2) 객체 생성으로 반환되는 참조 값을 MemberServiceImpl 객체를 생성하면서 생성자로 전달한다.
@@ -24,14 +34,17 @@ public class AppConfig {
 
     }
 
-    private static MemoryMemberRepositoryImpl memberRepositoryImpl() {
+    @Bean
+    public static MemoryMemberRepositoryImpl memberRepositoryImpl() {
         return new MemoryMemberRepositoryImpl();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepositoryImpl(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // 할인 정책 변경: FixDiscountPolicy -> RateDiscountPolicy
         // return new FixDiscountPolicy();
