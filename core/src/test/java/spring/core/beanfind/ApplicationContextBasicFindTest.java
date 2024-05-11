0b5ceffb9022b5668af694f865aeaf018221d7b2
soojin <pofoxz@gmail.com>
2024-05-12 08:49:03 +0900

    Test: 기본적인 스프링 빈 조회 - 빈 이름으로 조회

---
+++ core/src/test/java/spring/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,25 @@
+package spring.core.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService =
+                ac.getBean("memberService", MemberService.class);
+
+        // memberService(MemberService = interface)의 구현 객체를 판단한다.
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
