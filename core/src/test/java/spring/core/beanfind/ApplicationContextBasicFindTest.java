7c3711cc6c2367cdf45dffc2f1a3bdf1b5196946
soojin <pofoxz@gmail.com>
2024-05-12 09:02:30 +0900

    Test: 기본적인 스프링 빈 조회 - 조회할 빈이 존재하지 않는 경우

--- core/src/test/java/spring/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/spring/core/beanfind/ApplicationContextBasicFindTest.java
@@ -2,12 +2,14 @@ package spring.core.beanfind;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AppConfig;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationContextBasicFindTest {
     AnnotationConfigApplicationContext ac =
@@ -43,5 +45,15 @@ class ApplicationContextBasicFindTest {
 
         // memberService의 구현 객체를 판단한다.
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("조회할 빈이 존재하지 않는 경우")
+    void findBeanByNameX() {
+        // ac.getBean("xxxxx", MemberService.class); => "xxxxx"라는 이름의 빈은 존재하지 않는다.
+
+        // () -> : 우측에 지정한 로직을 실행했을 시, 지정한 예외가 발생하면 테스트에 성공한다.
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
     }
 }
