051875b03b67d1e60a51ed078dd8531b62f7068d
soojin <pofoxz@gmail.com>
2024-05-12 11:43:42 +0900

    Test: 스프링 빈 설정 메타 정보(BeanDefinition) 확인 - AppConfig.class

---
+++ core/src/test/java/spring/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,30 @@
+package spring.core.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+
+class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+        // 정의된 모든 스프링 빈을 출력한다.: 스프링 기본 빈 포함
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // getBeanDefinition(name): 각 빈에 대한 메타 데이터 정보를 가져온다.
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                // 해당 빈이 애플리케이션 빈인 경우, 출력한다.
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " / beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
