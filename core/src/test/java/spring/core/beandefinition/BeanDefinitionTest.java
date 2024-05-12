b9a35e069797a463e47385b2b4b9af1804e41c74
soojin <pofoxz@gmail.com>
2024-05-12 11:46:09 +0900

    Test: 스프링 빈 설정 메타 정보(BeanDefinition) 확인 - appConfig.xml

--- core/src/test/java/spring/core/beandefinition/BeanDefinitionTest.java
+++ core/src/test/java/spring/core/beandefinition/BeanDefinitionTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import spring.core.AppConfig;
 
 class BeanDefinitionTest {
-    AnnotationConfigApplicationContext ac =
-            new AnnotationConfigApplicationContext(AppConfig.class);
+//    AnnotationConfigApplicationContext ac =
+//            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    GenericXmlApplicationContext ac =
+            new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타 정보 확인")
