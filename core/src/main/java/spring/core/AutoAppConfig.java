443014c19e7dd3598a62afb9f0fc1e1d04d66281
sxzuzv <pofoxz@gmail.com>
2024-05-17 08:55:57 +0900

    Add: 탐색 위치와 기본 스캔 대상 - @ComponentScan의 basePackages, basePackageClasses 지정

--- core/src/main/java/spring/core/AutoAppConfig.java
+++ core/src/main/java/spring/core/AutoAppConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.FilterType;
 // => Configuration.class를 스프링 빈 등록에서 제외한다.
 // => @Configuration 애너테이션이 붙은 경우, 자동으로 컴포넌트 스캔의 대상이 되므로 충돌을 막기 위해 제외시킨다.
 @ComponentScan(
+        // basePackageClasses 생략 시, @ComponentScan 애너테이션이 붙은 설정 정보 클래스가 위치한 패키지가 스캔의 시작 위치가 된다.
         basePackages = "spring.core",               // 지정한 패키지를 포함하여 하위 패키지 모두를 스캔한다.
         basePackageClasses = AutoAppConfig.class,   // 지정한 클래스가 속한 패키지부터 스캔을 시작한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
