7f9aad37981ef9f427880af9593492241f73abe2
sxzuzv <pofoxz@gmail.com>
2024-05-17 09:35:20 +0900

    Add: 필터 - 클래스에 사용자 정의 애너테이션 부착

---
+++ core/src/test/java/spring/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package spring.core.scan.filter;
+
+@IncludeComponent   // 컴포넌트 스캔에 추가로 포함한다.
+public class BeanA {
+}
---
+++ core/src/test/java/spring/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package spring.core.scan.filter;
+
+@ExcludeComponent   // 컴포넌트 스캔에서 제외한다.
+public class BeanB {
+}
