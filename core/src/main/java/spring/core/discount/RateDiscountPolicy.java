9c23c8f83a91834ded9fd60b1bc274ada842dbe4
sxzuzv <pofoxz@gmail.com>
2024-05-22 11:54:37 +0900

    Add: 다수의 빈이 조회되는 경우 - @Qualifier 지정

--- core/src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,12 @@
 package spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
     // 주문한 금액 대비 %를 할인해주는 새로운 정률 할인 정책을 추가한다.
     private int discountPercent = 10;   // 할인율 10% 고정
