c86b60cc48668e02c556ef208601111cea6f4e7d
sxzuzv <pofoxz@gmail.com>
2024-05-22 14:36:13 +0900

    Add: 다수의 빈이 조회되는 경우 - @Primary 적용

--- core/src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,14 @@
 package spring.core.discount;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+//@Qualifier("mainDiscountPolicy")
+@Primary    // @Autowired 시, @Primary 애너테이션이 붙은 빈을 가장 높은 우선순위로 설정한다.
 public class RateDiscountPolicy implements DiscountPolicy {
     // 주문한 금액 대비 %를 할인해주는 새로운 정률 할인 정책을 추가한다.
     private int discountPercent = 10;   // 할인율 10% 고정
