c146121d55e1cdfb51786589c05b480e83af75ef
sxzuzv <pofoxz@gmail.com>
2024-05-23 11:25:34 +0900

    Test: 빈 생명 주기 테스트

---
+++ core/src/test/java/spring/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package spring.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest() {
+        // 스프링 컨테이너(싱글톤 컨테이너) 생성
+        // ConfigurableApplicationContext: AnnotationConfigApplicationContext의 상위 인터페이스
+        // => 부모는 자식을 담을 수 있다.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        ac.close();
+    }
+
+    @Configuration  // 설정 정보 클래스에 부착
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();  // 스프링 빈 등록 과정에서 객체 생성, 생성자가 호출된다.
+            networkClient.setUrl("http://hello-spring.dev");    // 임의의 URL 지정
+            return networkClient;   // 반환 객체가 스프링 빈으로 등록된다.
+        }
+    }
+}
