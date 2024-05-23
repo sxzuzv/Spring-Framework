3f2c99a59164b0135cd67529eeb502be62d5c229
sxzuzv <pofoxz@gmail.com>
2024-05-23 11:55:31 +0900

    Test: 인터페이스를 사용하는 초기화, 종료 - InitializingBean, DisposableBean

--- core/src/test/java/spring/core/lifecycle/NetworkClient.java
+++ core/src/test/java/spring/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package spring.core.lifecycle;
 
-public class NetworkClient {    // 테스트를 위한 클라이언트 객체
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {    // 테스트를 위한 클라이언트 객체
     private String url;
 
     public NetworkClient() {
         // NetworkClient 객체 생성 시, connect() 메서드 호출 + call() 메서드에 연결 메시지 전달
         System.out.println("생성자 호출! URL = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,20 @@ public class NetworkClient {    // 테스트를 위한 클라이언트 객체
     // 서비스 종료 시 호출 메서드: disconnect()
     public void disconnect() {
         System.out.println("CLOSE: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 프로퍼티 세팅이 끝나면(=의존 관계 주입이 끝나면) 호출된다.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 스프링 빈 소멸 직전에 호출된다.
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
